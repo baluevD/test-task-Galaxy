c2639c72b66c88929c79d74307a92f15923f5c27
baluevD <newarmlet@gmail.com>
2018-10-24 13:51:54 +0300

    add jquery by npm

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
     // $(".accordion h3").click(function(){
     //     $(this).next("p").slideToggle("slow")
     //         .siblings("p:visible").slideUp("slow");
