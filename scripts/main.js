9d0afde942373398cdadca2b82b1999330e5b988
baluevD <newarmlet@gmail.com>
2018-11-01 13:23:35 +0300

    fix options, name $ link to jQuery

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,17 @@
 $(function() {
   var icons = {
-    header: "iconClosed",    // custom icon class
+    header: "iconClosed", // custom icon class
     activeHeader: "iconOpen" // custom icon class
   };
-  $( "#accordion" ).accordion({
+  $("#accordion").accordion({
     icons: icons,
-    heightStyle: "content"
+    heightStyle: "fill"
   });
-} );
+  $("#accordion-resizer").resizable({
+    minHeight: 140,
+    minWidth: 176,
+    resize: function() {
+      $("#accordion").accordion("refresh");
+    }
+  });
+})(jQuery);
